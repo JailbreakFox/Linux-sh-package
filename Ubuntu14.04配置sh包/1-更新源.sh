9ee7519f1734326de54e61f6735ddc04fb4afd37
xuyanghe <tracerxu@126.com>
2019-08-13 12:47:25 +0800

    add rtl8812au

--- Ubuntu14.04配置sh包/1-更新源.sh
+++ Ubuntu14.04配置sh包/1-更新源.sh
@@ -9,31 +9,31 @@ sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 echo "" > /etc/apt/sources.list
 
 #往sources.list添加国内源（清华）----Ubuntu14.04
-echo "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-updates main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-updates main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-backports main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-backports main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-security main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-security main restricted universe multiverse
+# echo "# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty main restricted universe multiverse
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-updates main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-updates main restricted universe multiverse
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-backports main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-backports main restricted universe multiverse
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-security main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-security main restricted universe multiverse
  
-# 预发布软件源，不建议启用
-# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-proposed main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-proposed main restricted universe multiverse" > /etc/apt/sources.list
+# # 预发布软件源，不建议启用
+# # deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-proposed main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ trusty-proposed main restricted universe multiverse" > /etc/apt/sources.list
 #往sources.list添加国内源（清华）----Ubuntu16.04
-#echo "# deb cdrom:[Ubuntu 16.04 LTS _Xenial Xerus_ - Release amd64 (20160420.1)]/ xenial main restricted
-#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial main restricted
-#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates main restricted
-#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial universe
-#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates universe
-#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial multiverse
-#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates multiverse
-#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
-#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security main restricted
-#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security universe
-#deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security multiverse" > /etc/apt/sources.list
+echo "# deb cdrom:[Ubuntu 16.04 LTS _Xenial Xerus_ - Release amd64 (20160420.1)]/ xenial main restricted
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial main restricted
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates main restricted
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial universe
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates universe
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial multiverse
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates multiverse
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security main restricted
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security universe
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security multiverse" > /etc/apt/sources.list
 
 #更新源
 sudo apt-get update
---
+++ Ubuntu14.04配置sh包/rtl8812au.sh
@@ -0,0 +1,15 @@
+#运行方式
+#sh rtl8812au.sh
+
+#===========rtl8812au安装============
+#安装。
+#sudo apt-get install linux-headers-generic build-essential git libelf-dev
+
+#下载源码
+#git clone https://github.com/diederikdehaas/rtl8812AU.git
+#(如果上面的源码不能用，则直接用商家提供的源码，在压缩包中)：
+#https://github.com/SailCPU/ForceControlGroup/tree/master/file/rtl8812au驱动ubuntu16.04
+
+#进入源码包make并安装
+#make
+#sudo make install
