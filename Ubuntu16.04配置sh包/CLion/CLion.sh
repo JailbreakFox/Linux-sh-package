f8f61b592404c7655e8c821fbe73eaefbcddb0f7
xuyanghe <tracerxu@126.com>
2019-08-13 16:39:04 +0800

    modify clion

--- Ubuntu16.04配置sh包/CLion/CLion.sh
+++ Ubuntu16.04配置sh包/CLion/CLion.sh
@@ -1,11 +1,14 @@
 #运行方式
 #sh CLion.sh
 
-#===========CLion安装==========
+#===========CLion 2018.1.5安装==========
+#安装必要开发环境
+sudo apt-get install build-essential
+
 #下载CLion包https://www.jetbrains.com/clion/
 #解压tar.gz包，解压位置推荐设置为用户目录下的opt文件~/opt
-tar -zxvf CLion-2019.1.3.tar.gz -C ~/opt
+tar -zxvf CLion-2018.1.5.tar.gz -C ~/opt
 #运行clion.sh脚本
-cd ~/opt/clion-2019.1.3/bin/
+cd ~/opt/clion-2018.1.5/bin/
 ./clion.sh
 
