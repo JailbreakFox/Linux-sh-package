8791b39e0e9607835f6181b699b2c00b12c469f5
xuyanghe <tracerxu@126.com>
2019-08-14 17:15:00 +0800

    add GooglePinYin

---
+++ Ubuntu16.04配置sh包/GooglePinYin.sh
@@ -0,0 +1,16 @@
+#运行方式
+#sh GooglePinYin.sh
+
+#===========GooglePinYin安装============
+#1.安装汉语语言包
+sudo apt-get install language-pack-zh-hans
+
+#2.安装谷歌拼音输入法
+sudo apt-get install fcitx-googlepinyin
+
+#3.打开SystemSettings-->Language Support
+#选择“键盘输入方式系统”为：fcitx
+
+#4.打开终端，执行“fcitx-configtool”命令
+
+#5.增加谷歌拼音输入法
