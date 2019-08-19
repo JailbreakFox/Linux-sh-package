20164b524484ea5b55f9be69cf02bbf91c4ebc86
xuyanghe <tracerxu@126.com>
2019-08-19 14:05:06 +0800

    add eagleye

---
+++ Ubuntu16.04配置sh包/Cobot/Cobot3DCalibration.sh
@@ -0,0 +1,12 @@
+#自研相机的快速标定软件
+#运行方式
+#sh Cobot3DCalibration.sh
+
+#============Cobot3DCalibration安装=============
+#进入bin文件夹
+
+#获取权限
+sudo chmod +x Cobot3DCalibration
+
+#安装
+./Cobot3DCalibration
---
+++ Ubuntu16.04配置sh包/Cobot/Comatrix.sh
@@ -0,0 +1,9 @@
+#运行方式
+#sh Comatrix.sh
+
+#============Comatrix安装=============
+#获取权限
+chmod +x ComatrixInstaller-1.9.0-Ubuntu14_20190221.run
+
+#安装
+sudo ./ComatrixInstaller-1.9.0-Ubuntu14_20190221.run
