9c2b97c5b719a05f1616dbe280bfb1a88e537b70
xuyanghe <tracerxu@126.com>
2019-08-31 20:18:00 +0800

    modify Nodejs

--- Ubuntu16.04配置sh包/Nodejs.sh
+++ Ubuntu16.04配置sh包/Nodejs.sh
@@ -2,22 +2,10 @@
 #sh Nodejs.sh
 
 #===========Nodejs安装============
-#执行检查可更新的软件
-sudo apt-get update
+#去官网下载最新的安装包
+#http://nodejs.cn/download/
 
-#安装低版本的Nodejs
-sudo apt-get install nodejs
-sudo apt install nodejs-legacy
-sudo apt install npm
-
-#更换淘宝的镜像(必须)
-sudo npm config set registry https://registry.npm.taobao.org
-
-#查看配置是否生效
-sudo npm config list
-
-#安装更新版本的工具N
-sudo npm install n -g
-
-#更新Nodejs版本
-sudo n stable
+#解压并进入安装包，依次执行
+./configure
+make
+make install
