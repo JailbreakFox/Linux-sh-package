3b9a495449497301f385cfb8217d27a915e1f36f
xuyanghe <tracerxu@126.com>
2019-09-08 12:39:12 +0800

    add docker

---
+++ Ubuntu16.04配置sh包/Docker.sh
@@ -0,0 +1,19 @@
+#运行方式
+#sh Docker.sh
+
+#===========Docker安装============
+#更新源
+sudo apt-get update
+
+#安装Docker
+apt-get install docker
+
+#安装Docker.io
+apt-get install docker.io
+
+#安装docker-registry。
+apt-get install docker-registry
+
+#启动Docker
+systemctl start docker
+
