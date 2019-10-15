90e1e70c3a1e02f9a216ec7fc87830f2388c07cb
xuyanghe <tracerxu@126.com>
2019-10-15 17:32:08 +0800

    add meshlab/typera

---
+++ Ubuntu16.04配置sh包/MeshLab.sh
@@ -0,0 +1,10 @@
+#运行方式
+#sh MeshLab.sh
+
+#===========MeshLab安装============
+#使用别人编译好的源
+sudo add-apt-repository ppa:zarquon42/meshlab
+sudo apt-get update
+
+#安装MeshLab
+sudo apt-get install meshlab
---
+++ Ubuntu16.04配置sh包/Typora.sh
@@ -0,0 +1,15 @@
+#运行方式
+#sh Typora.sh
+
+#===========Typora安装============
+#添加key
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCF
+
+#添加repository
+sudo add-apt-repository 'deb http://typora.io linux/'
+
+#更新apt-get
+sudo apt-get update
+
+#安装Typora
+sudo apt-get install typora
