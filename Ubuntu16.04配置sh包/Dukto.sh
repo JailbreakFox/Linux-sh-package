c44851717a35dbb15100783f785cd70968f821aa
xuyanghe <tracerxu@126.com>
2019-10-18 15:28:38 +0800

    add dukto&mNetAssist

---
+++ Ubuntu16.04配置sh包/Dukto.sh
@@ -0,0 +1,6 @@
+#一款用于局域网传输文件的工具
+#运行方式
+#sh Dukto.sh
+
+#===========Dukto安装============
+sudo dpkg -i dukto_6.0-1_amd64.deb
---
+++ Ubuntu16.04配置sh包/mNetAssist.sh
@@ -0,0 +1,6 @@
+#一款网口通信助手工具
+#运行方式
+#sh mNetAssist.sh
+
+#===========Dukto安装============
+sudo dpkg -i mNetAssist-release-amd64.deb
