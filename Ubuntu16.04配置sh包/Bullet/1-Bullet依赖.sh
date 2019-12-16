57bad1cdc19d2fd92b29b909b83c8ebf5a197e94
xuyanghe <tracerxu@126.com>
2019-12-16 09:28:48 +0800

    add bullet3

---
+++ Ubuntu16.04配置sh包/Bullet/1-Bullet依赖.sh
@@ -0,0 +1,10 @@
+#运行方式
+#sh 1-Bullet依赖.sh
+
+#===========Bullet依赖安装==========
+#编译工具
+#安装cmake
+sudo apt-get install  cmake
+#安装python3-dev
+sudo apt-get install  python-dev  python3-dev
+
---
+++ Ubuntu16.04配置sh包/Bullet/2-Bullet安装.sh
@@ -0,0 +1,7 @@
+#运行方式
+#该操作需要手动进行
+
+#===========Bullet安装==========
+#从Git仓库中下载Bullet3源码
+#git clone https://github.com/bulletphysics/bullet3.git
+
