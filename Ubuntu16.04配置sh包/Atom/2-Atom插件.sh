bf5aacf98c5bb81583c091349c7b4f7b17a8dafe
xuyanghe <tracerxu@126.com>
2019-12-16 16:59:31 +0800

    modify atom

---
+++ Ubuntu16.04配置sh包/Atom/2-Atom插件.sh
@@ -0,0 +1,13 @@
+#运行方式
+#该操作需要手动进行
+
+#===========Atom常用插件安装==========
+#Markdown显示插件
+#markdown-preview-enhanced
+#该插件能显示流程图，但是有些方程式显示不出来
+
+#markdown-preview-plus
+#该插件能显示方程式，但是有些流程图显示不出来
+
+#markdown-pdf
+#该插件能将md转换为pdf
