16882903697ecdf5bc151ffbf348fb6728756999
xuyanghe <tracerxu@126.com>
2020-01-30 23:02:03 -0800

    add pytorch

---
+++ Ubuntu16.04配置sh包/PyTorch/1-PyTorch配置工具.sh
@@ -0,0 +1,7 @@
+#运行方式
+#sh 1-PyTorch配置工具.sh
+
+#先安装pip3
+sudo apt-get install python3-pip
+#更新pip3版本
+sudo pip3 install --upgrade pip
--- Ubuntu16.04配置sh包/PyTorch/2-PyTorch.sh
+++ Ubuntu16.04配置sh包/PyTorch/2-PyTorch.sh
@@ -1,6 +1,8 @@
 #运行方式
-#sh 2-PyTorch配置工具.sh
+#sh PyTorch.sh
 
+#===========PyTorch安装============
+#去官网https://pytorch.org/查安装代码
 pip install torch==1.4.0+cpu torchvision==0.5.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
 #numpy
