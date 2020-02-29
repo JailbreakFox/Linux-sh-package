81fa6e8715fbce6d1d72b462d338c3832f1be803
xuyanghe <tracerxu@126.com>
2020-02-28 18:56:34 -0800

    add 视频工具

---
+++ Ubuntu16.04配置sh包/视频工具/you-get.sh
@@ -0,0 +1,10 @@
+#运行方式
+#sh you-get.sh
+
+#you-get是网上视频内容下载工具,比如B站视频
+#===========you-get安装============
+#安装you-get
+pip3 install you-get
+
+#下载视频,比如
+#you-get https://www.bilibili.com/video/av86889021/
