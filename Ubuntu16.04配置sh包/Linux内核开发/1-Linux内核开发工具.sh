3c6ad32ea0c7c93851675d205045427cb561617c
xuyanghe <tracerxu@126.com>
2020-03-08 23:14:13 -0700

    add 内核开发

---
+++ Ubuntu16.04配置sh包/Linux内核开发/1-Linux内核开发工具.sh
@@ -0,0 +1,7 @@
+#Linux内核开发的工具安装
+#摘自B站<<奔跑吧Linux内核>>系列视频
+#运行方式
+#sh 1-Linux内核开发工具.sh
+
+#===========Linux内核开发工具安装============
+sudo apt-get install qemu libncurses5-dev gcc-arm-linux-gnueabi build-essential gdb-arm-none-eabi gcc-aarch64-linux-gnu eclipse-cdt git
---
+++ Ubuntu16.04配置sh包/视频工具/FFmpeg/2-FFmpeg.sh
@@ -0,0 +1,5 @@
+#运行方式
+#sh 2-FFmpeg.sh
+
+#===========FFmpeg安装============
+sudo apt install ffmpeg
