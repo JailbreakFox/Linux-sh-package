ed23d8f1332c0853d4b1971bb7c15af0a4db0846
JailbreakFox <tracerxu@126.com>
2020-03-14 12:24:12 +0800

    modify youget

--- Ubuntu16.04-LTS-sh/视频工具/you-get/you-get.sh
+++ Ubuntu16.04-LTS-sh/视频工具/you-get/you-get.sh
@@ -4,7 +4,7 @@
 #you-get是网上视频内容下载工具,比如B站视频
 #===========you-get安装============
 #安装you-get
-pip3 install you-get
+sudo pip3 install you-get
 
 #下载视频,比如
 #you-get https://www.bilibili.com/video/av86889021/
