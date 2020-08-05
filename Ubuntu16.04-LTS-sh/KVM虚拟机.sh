8c5a41685a5faa30ea0df554dbf69b73802fab50
xuyanghe <xuyanghe@uniontech.com>
2020-08-05 17:02:28 +0800

    add KVM

---
+++ Ubuntu16.04-LTS-sh/KVM虚拟机.sh
@@ -0,0 +1,5 @@
+#运行方式
+#sh KVM虚拟机.sh
+
+#===========KVM虚拟机安装============
+sudo apt -y install libvirt0 libvirt-daemon qemu virt-manager bridge-utils libvirt-clients python-libvirt qemu-efi uml-utilities virtinst qemu-system
