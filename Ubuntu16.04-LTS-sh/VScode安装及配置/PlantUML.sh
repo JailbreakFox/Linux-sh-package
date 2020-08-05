157b42014d4265692e72e27a16e99e32079c3623
xuyanghe <xuyanghe@uniontech.com>
2020-08-05 11:27:50 +0800

    add PlantUML

--- Ubuntu16.04-LTS-sh/VScode安装及配置/PlantUML.sh
+++ Ubuntu16.04-LTS-sh/VScode安装及配置/PlantUML.sh
@@ -1,2 +1,9 @@
 #===========PlantUML安装============
-# 直接在VSCode扩展库中找到PlantUML
+# ----- 先安装Java -----
+# 寻找库中的jdk文件
+apt-cache search jdk
+# 直接安装找到的包
+sudo apt install openjdk-11-jdk
+
+# 直接在VSCode扩展库中找到PlantUML,安装
+# 写好*.wsd, *.pu, *.puml, *.plantuml, *.iuml 文件以后,按 ALT+D 显示
