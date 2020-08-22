9e4a7eccf279794b8f42d270de207f262cff4233
xuyanghe <xuyanghe@uniontech.com>
2020-08-22 09:07:39 +0800

    add Hexo

---
+++ Ubuntu16.04-LTS-sh/博客搭建/Hexo.sh
@@ -0,0 +1,24 @@
+#运行方式
+#sh Hexo.sh
+
+#===========Hexo安装============
+# 先安装nodejs和npm
+# 确保源内能找到以上两个包
+sudo apt install nodejs npm
+
+# 用npm安装hexo
+sudo npm install hexo-cli -g
+
+# 初始化hexo框架
+# hexo init
+# 启动本地服务
+# hexo s
+#新建文章
+# hexo new "postName" 
+
+# 清除缓存
+# hexo clean
+# 编译生成静态页面
+# hexo generate = hexo g
+# 部署到GitHub
+# hexo deploy = hexo d
