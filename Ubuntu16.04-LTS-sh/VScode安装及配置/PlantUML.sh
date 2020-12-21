de64836b3f25ae8dde1675924f3f86d905642e62
xuyanghe <xuyanghe@uniontech.com>
2020-12-21 17:53:30 +0800

    modify plantuml and dia

--- Ubuntu16.04-LTS-sh/VScode安装及配置/PlantUML.sh
+++ Ubuntu16.04-LTS-sh/VScode安装及配置/PlantUML.sh
@@ -5,5 +5,8 @@ apt-cache search jdk
 # 直接安装找到的包
 sudo apt install openjdk-11-jdk
 
+# ----- 再安装graphviz -----
+sudo apt install graphviz
+
 # 直接在VSCode扩展库中找到PlantUML,安装
 # 写好*.wsd, *.pu, *.puml, *.plantuml, *.iuml 文件以后,按 ALT+D 显示
--- Ubuntu16.04-LTS-sh/绘图工具/Dia.sh
+++ Ubuntu16.04-LTS-sh/绘图工具/Dia.sh
@@ -4,7 +4,9 @@
 #Dia是跨平台的流程图绘制工具
 #===========Dia安装============
 #安装Dia
-sudo apt-get install dia-common
+sudo apt-get install dia
+#在终端启动时增加启动设置，将输入法改为中文
+export GTK_IM_MODULE=xim
 
 #打开Dia
 dia
